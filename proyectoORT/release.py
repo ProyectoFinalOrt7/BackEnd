3c7d9bde9c843888a66cb01f37c28f570aeecc01
Sebastian Carreira <sebastian.m.carreira@gmail.com>
2021-10-13 20:43:12 -0300

    Fixed fb config file creation position

--- proyectoORT/release.py
+++ proyectoORT/release.py
@@ -1,5 +1,5 @@
 import os
-with open('proyectoORT/fbAdminConfig.json', 'w') as fb_config_file:
+with open('fbAdminConfig.json', 'w') as fb_config_file:
     fb_config_file.write(os.environ['FB_ADMIN_CONFIG'])
 
 from django.core.management import execute_from_command_line
