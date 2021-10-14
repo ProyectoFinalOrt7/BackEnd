69636d370b3e7154e0fa3f7e633e024984225b04
Sebastian Carreira <sebastian.m.carreira@gmail.com>
2021-10-13 23:21:21 -0300

    release script logging

--- proyectoORT/release.py
+++ proyectoORT/release.py
@@ -1,10 +1,10 @@
 import os
 
-print(os.listdir())
+print(os.listdir('proyectoORT'))
 print("Creating Firebase config file from env var FB_ADMIN_CONFIG")
-with open('fbAdminConfig.json', 'w') as fb_config_file:
+with open('proyectoORT/fbAdminConfig.json', 'w') as fb_config_file:
     fb_config_file.write(os.environ['FB_ADMIN_CONFIG'])
-print(os.listdir())
+print(os.listdir('proyectoORT'))
 
 from django.core.management import execute_from_command_line
 
