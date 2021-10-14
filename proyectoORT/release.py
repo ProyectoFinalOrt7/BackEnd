516048953bc316ed13d7ee043e60a6a197556dae
Sebastian Carreira <sebastian.m.carreira@gmail.com>
2021-10-13 23:18:22 -0300

    release script logging

--- proyectoORT/release.py
+++ proyectoORT/release.py
@@ -1,9 +1,19 @@
 import os
+
+print(os.listdir())
+print("Creating Firebase config file from env var FB_ADMIN_CONFIG")
 with open('fbAdminConfig.json', 'w') as fb_config_file:
     fb_config_file.write(os.environ['FB_ADMIN_CONFIG'])
+print(os.listdir())
 
 from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoORT.settings')
+print("Applying migrations")
 execute_from_command_line(['manage.py', 'migrate'])
-execute_from_command_line(['manage.py', 'createsuperuser', '--no-input', '--username', os.environ['ADMIN_USERNAME'], '--email', 'noemail@example.com'])
+
+if os.environ['CREATE_SUPERUSER']:
+    print("Creating superuser")
+    execute_from_command_line(['manage.py', 'createsuperuser', '--no-input', '--username', os.environ['ADMIN_USERNAME'], '--email', 'noemail@example.com'])
+else:
+    print("Skipping superuser creation")
