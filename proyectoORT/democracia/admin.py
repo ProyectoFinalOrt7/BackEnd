ecf791f0906f6e3ca8a2d60e30129f713affebd9
Sebastian Carreira <sebastian.m.carreira@gmail.com>
2021-11-07 01:10:15 -0300

    Lifecycle de encuestas

--- proyectoORT/democracia/admin.py
+++ proyectoORT/democracia/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models.auth import Ciudadano
-from .models.democracia import Partido, Distrito, Idea, Categoria, Voto
+from .models.democracia import Partido, Distrito, Idea, Categoria, Voto, Encuesta, VotoEncuesta, OpcionEncuesta
 
 admin.site.register(Ciudadano)
 admin.site.register(Partido)
@@ -8,4 +8,7 @@ admin.site.register(Distrito)
 admin.site.register(Idea)
 admin.site.register(Categoria)
 admin.site.register(Voto)
+admin.site.register(Encuesta)
+admin.site.register(VotoEncuesta)
+admin.site.register(OpcionEncuesta)
 # Register your models here.
